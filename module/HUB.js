83accef2c850a7dd86875a10a7c0b2b6b3f8dfb1
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-11-02 17:24:18 -0700

    lab done

--- module/HUB.js
+++ module/HUB.js
@@ -1,27 +1,40 @@
 'use strict';
 
-const hub = require('../eventPool');
-require('./vendor/vendor');
-require('./driver/driver');
+const socketio = require('socket.io');
+const PORT = process.env.PORT || 3030;
+const server = socketio(PORT);
 const date = new Date();
 let time = date.toString();
 
-hub.on('pick-up', (payload) =>
-	console.log(
-		`EVENT {event: pick-up, time: ${time}, payload: ${JSON.stringify(payload)}}`
-	)
-);
-hub.on('in-transit', (payload) =>
-	console.log(
-		`EVENT {event: in-transit, time: ${time}, payload: ${JSON.stringify(
-			payload
-		)}}`
-	)
-);
-hub.on('delivered', (payload) =>
-	console.log(
-		`EVENT {event: delivered, time: ${time}, payload: ${JSON.stringify(
-			payload
-		)}}`
-	)
-);
+const caps = server.of('/caps');
+
+caps.on('connection', (socket) => {
+	console.log('socket is connected on', socket.id);
+	socket.on('join', (payload) => {
+		socket.join(payload);
+	});
+	socket.on('pick-up', (payload) => {
+		console.log(
+			`EVENT {event: pick-up, time: ${time}, payload: ${JSON.stringify(
+				payload
+			)}}`
+		);
+		caps.emit('pick-up', payload);
+	});
+	socket.on('in-transit', (payload) => {
+		console.log(
+			`EVENT {event: in-transit, time: ${time}, payload: ${JSON.stringify(
+				payload
+			)}}`
+		);
+		caps.emit('in-transit', payload);
+	});
+	socket.on('delivered', (payload) => {
+		console.log(
+			`EVENT {event: delivered, time: ${time}, payload: ${JSON.stringify(
+				payload
+			)}}`
+		);
+		caps.emit('delivered', payload);
+	});
+});
--- module/driver/driver.js
+++ module/driver/driver.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const { moduleExpression } = require('@babel/types');
-const driver = require('../../eventPool');
-require('../vendor/vendor.js');
-require('../HUB');
-
-// -------------------------- Testing
+const client = require('socket.io-client');
+const CAPS = client.connect('http://localhost:3030/caps');
 
 let handleDelivery = (payload) => {
 	console.log(`DRIVER: picked up ${payload.orderId}`);
-	driver.emit('in-transit', payload);
-	driver.emit('delivered', payload);
+	CAPS.emit('in-transit', payload);
+	CAPS.emit('delivered', payload);
 };
 
-driver.on('pick-up', handleDelivery);
+CAPS.on('pick-up', handleDelivery);
 
 module.exports = handleDelivery;
